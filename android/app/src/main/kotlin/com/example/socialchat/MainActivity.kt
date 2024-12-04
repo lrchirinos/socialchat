991d3adf7a387b9e07e5a726854e3fe908d32802
Rolando <yutipu@gmail.com>
2024-12-04 18:03:55 -0500

    Primer avance

---
+++ android/app/src/main/kotlin/com/example/socialchat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.socialchat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+//Pages
+import './pages/splash_page.dart';
+
+void main() {
+  runApp(
+    SplashPage(
+      key: UniqueKey(),
+      onInitializationComplete: () {},
+    ),
+  );
+}
+
