b9f3633d8f0c7af8869cf86895ab92eec213c920
Rolando <yutipu@gmail.com>
2024-12-04 22:22:45 -0500

    Terminado splash

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,35 @@ void main() {
   runApp(
     SplashPage(
       key: UniqueKey(),
-      onInitializationComplete: () {},
+      onInitializationComplete: () {
+        runApp(
+          MainApp(),
+        );
+      },
     ),
   );
 }
 
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SocialChat',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Color.fromRGBO(36, 35, 49, 1.0),
+          background: Color.fromRGBO(36, 35, 49, 1.0),
+        ),
+        scaffoldBackgroundColor: Color.fromRGBO(36, 35, 49, 1.0),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Color.fromRGBO(30, 29, 37, 1.0),
+        ),
+      ),
+      // home: Scaffold(
+      //   appBar: AppBar(
+      //     title: Text('SocialChat'),
+      //   ),
+      // ),
+    );
+  }
+}
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+//Packages
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get_it/get_it.dart';
+
+//Services
+import '../services/navigation_service.dart';
+import '../services/media_service.dart';
+import '../services/cloud_storage_service.dart';
+import '../services/database_service.dart';
+
+
 class SplashPage extends StatefulWidget {
   final VoidCallback onInitializationComplete;
 
@@ -18,6 +29,13 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
+    Future.delayed(Duration(seconds: 1)).then(
+      (_) {
+        _setup().then(
+          (_) => widget.onInitializationComplete(),
+        );
+      },
+    );
   }
  
   @override
@@ -45,6 +63,26 @@ class _SplashPageState extends State<SplashPage> {
           ),
         ),
       ),
+    );
+  }
+  Future<void> _setup() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    _registerServices();
+  }
+
+  void _registerServices() {
+    GetIt.instance.registerSingleton<NavigationService>(
+      NavigationService(),
+    );
+    GetIt.instance.registerSingleton<MediaService>(
+      MediaService(),
+    );
+    GetIt.instance.registerSingleton<CloudStorageService>(
+      CloudStorageService(),
+    );
+    GetIt.instance.registerSingleton<DatabaseService>(
+      DatabaseService(),
     );
   }
 
